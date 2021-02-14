2a6d357ac0811280e3b76377426e6f05408179b5
Colten Appleby <colten96@gmail.com>
2021-02-14 21:27:56 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,78 +8,22 @@
 
 
 
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
 
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end 
 
+def introduction_with_language_optional(name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+introduction("colten")
+introduction_with_language("colten", "python")
+introduction_with_language_optional("colten")
 
 
 
